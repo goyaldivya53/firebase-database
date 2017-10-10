8f971e085f24c95327b83b695a442f3e6e56e3b6
Divya Goyal <divya.goyal@CALSOFTHQ.COM>
2017-10-10 16:34:55 +0530

    form

---
+++ public/index.js
@@ -0,0 +1,42 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCs3tVUKeiPefk05WhZg_7T8dktD95EGkg",
+    authDomain: "web-start-e0f28.firebaseapp.com",
+    databaseURL: "https://web-start-e0f28.firebaseio.com",
+    projectId: "web-start-e0f28",
+  };
+  firebase.initializeApp(config);
+
+  
+ 
+
+  //Get a reference to the database service
+  var db=firebase.database();
+  
+  //onclick function
+  function signMe(name,email,city,phone){
+     
+    //get elements by id
+    var name=document.getElementById("form-name").value;
+    var email=document.getElementById("form-email").value;
+    var city=document.getElementById("form-city").value;
+    var phone=document.getElementById("form-phone").value;
+
+    //Replacing . by comma
+    let emailComma = email.replace(/\./g, ',');
+    let ref = db.ref('email/' + emailComma);
+    //storing the values
+     ref.set({
+        name:name,
+        email:email,
+        city:city,
+        phone:phone
+    }, function(err){
+        if (err) {
+            alert("something went wrong!")
+        
+        } else {
+           alert("thank you for registeration")
+        }
+    });
+  }
