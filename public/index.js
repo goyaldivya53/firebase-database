84d2938770d060f21e923f6ce80ef7b76f67b830
Divya Goyal <divya.goyal@CALSOFTHQ.COM>
2017-10-11 11:36:31 +0530

    Radio button

--- public/index.js
+++ public/index.js
@@ -14,14 +14,14 @@
   var db=firebase.database();
   
   //onclick function
-  function signMe(name,email,city,phone){
+  function signMe(name,email,city,phone,gender){
      
     //get elements by id
     var name=document.getElementById("form-name").value;
     var email=document.getElementById("form-email").value;
     var city=document.getElementById("form-city").value;
     var phone=document.getElementById("form-phone").value;
-
+    var gender=document.querySelector('input[name="gender"]:checked').value;
     //Replacing . by comma
     let emailComma = email.replace(/\./g, ',');
     let ref = db.ref('email/' + emailComma);
@@ -30,7 +30,8 @@
         name:name,
         email:email,
         city:city,
-        phone:phone
+        phone:phone,
+        gender:gender
     }, function(err){
         if (err) {
             alert("something went wrong!")
